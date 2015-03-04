e4880fc39bda2a543549dac0a17ec503dc3b01fa
Viet H Nguyen <viet.nguyen182@gmail.com>
2015-03-03 22:57:36 -0500

    Complete HW08

--- HW08.hs
+++ HW08.hs
@@ -56,3 +56,48 @@ battleResults x y = change <> (battleResults x_remainder y_remainder)
         change 
             | x1 > y1 = ArmyCounts {attackers = 0, defenders = -1} 
             | otherwise = ArmyCounts{attackers = -1, defenders = 0} 
+
+-- Exercise 5 
+battle :: ArmyCounts -> StdRand ArmyCounts 
+battle (ArmyCounts {attackers = a, defenders = d}) = do 
+    nAttacks <- getRandomR (1, (min (a - 1) 3) )    -- random number of units for attackers
+    nDefends <- getRandomR (1, (min (d - 1) 3) )    -- random number of units for defenders
+    attackRolls <- dice nAttacks    -- get the list of random rolls
+    defenseRolls <- dice nDefends   -- get the list of random rolls
+    let result = battleResults attackRolls defenseRolls   -- calculate the outcome of this battle
+    return ArmyCounts {attackers = a + (attackers result), defenders = d + (defenders result)}  -- caculate the new unit number for both.
+
+
+-- The dice function uses replicate and sequence to simulate the roll of n dice 
+-- We already have the dieRoll of the type `StdRand DieRoll`, so here we only needs to replicate them and sequence together
+-- to get just one monadic value 
+dice :: Int -> StdRand [DieRoll]
+dice n = sequence (replicate n dieRoll) 
+
+-- To extract a value from the Rand monad, we can use the `evalRandIO`
+-- Example: evalRandIO (dice  4)
+
+-- Exercise 6 
+invade :: ArmyCounts -> StdRand ArmyCounts 
+invade (ac@ArmyCounts {attackers = a, defenders = d}) 
+    | d == 0 = return ac
+    | a < 2 = return ac 
+    | otherwise = do
+        newac <- battle ac
+        invade newac 
+
+-- Exercise 7 
+successProb :: ArmyCounts -> StdRand Double 
+successProb ac = go ac 1000 0
+    where go ac 0 y = return (y / 1000)
+          go ac n y = do 
+            finalac <- invade ac 
+            if (defenders finalac == 0) then
+                go ac (n-1) (y+1)
+            else
+                go ac (n-1) y 
+
+
+
+(//) :: Int -> Int -> Double 
+a // b = fromIntegral a / fromIntegral b
